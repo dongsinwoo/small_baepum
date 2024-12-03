3beb6f2e359a84e1057c8056773021e66597cdcc
dong woo sin <109775028+dongsinwoo@users.noreply.github.com>
2024-12-03 10:12:38 +0900

    [오류수정]머지 오류

--- baepum/src/App.js
+++ baepum/src/App.js
@@ -1,27 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
-import MainScreen from './MainScreen';
-import LoginScreen from './LoginScreen';
-import PaymentScreen from './PaymentScreen';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainScreen />} />
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/home" element={<MainScreen />} />
-        <Route path="/payment" element={<PaymentScreen />} />
-      </Routes>
-    </Router>
-=======
 import MainScreen from './pages/MainScreen';
 import LoginScreen from './pages/LoginScreen';
 import PaymentScreen from './pages/PaymentScreen';
 import MyScreen from './pages/MyScreen';
 import NavBar from './components/NavBar';
 import { createGlobalStyle } from 'styled-components';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -44,7 +27,6 @@ function App() {
         <NavBar />
       </Router>
     </>
->>>>>>> c106d38 ([신동우]: 전체적으로 검토 및 수정 작업 진행(api: 추가, 메인페이지: 수정, 사용자페이지: 추가))
   );
 }
 
